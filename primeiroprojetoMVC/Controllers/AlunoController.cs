7e6ba4dcf0ef21e0904801192bd137a167cd9e82
higoferreira09 <higoferreira.dev@gmail.com>
2024-08-06 21:33:21 -0300

    atualização aula 06/08/2024

---
+++ primeiroprojetoMVC/Controllers/AlunoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace primeiroprojetoMVC.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
