c470bd9775a5c2d032ee889ab4a40a624fd5b00e
higoferreira97 <higoferreira.dev@gmail.com>
2024-08-15 21:47:42 -0300

    Aula 15/08/2024 v2

--- primeiroprojetoMVC/Models/Aluno.cs
+++ primeiroprojetoMVC/Models/Aluno.cs
@@ -6,7 +6,7 @@
         public string Matricula { get; set; }
         public string Nome { get; set; }
         public string Cpf { get; set; }
-        public string DataDeNascimento { get; set; }
+        public DateOnly DataDeNascimento { get; set; }
         public string Endereco { get; set; }
     }
 }
